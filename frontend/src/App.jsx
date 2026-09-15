8e736e67cf8c772c26a4ac14f227ab0b0440af59
Dang Nguyen <dangnguyen1818@gmail.com>
2026-09-15 16:44:12 -0700

    Initialized React Vite Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Sports Betting Prediction App</h1>
+      <p>Premier League | NBA | NFL</p>
+    </div>
+  );
+}
+
+export default App;
